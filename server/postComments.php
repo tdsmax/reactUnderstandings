daaaa830557c625f03f41e558fe26b1967556195
tarandeep singh <tarandeep.singh@bonavitatech.com>
2016-01-27 18:51:59 +0530

    made changes to the server side logic

--- server/postComments.php
+++ server/postComments.php
@@ -1,6 +1,6 @@
 <?php
 $myfile = fopen("..\\js\\\json\\data.json", "c") or die("Unable to open file!");
-$values = ",{ \"idx\": \"6\",  \"author\": \"Tds\",  \"text\": \"Commented\"}" . "]";
+$values = ",{ \"idx\": \"6\",  \"author\": \"Singh Piara Aakhde\",  \"text\": \"Commented\"}" . "]";
 /*foreach($_POST as $key=>$value) {
 	echo $key;
 	echo $value;
